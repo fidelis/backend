1d9841891475be2ddcabb0c1455fd5f3e4984829
Marcelo <fidelis.marcelo@gmail.com>
2020-10-15 18:43:58 -0300

    My second commit

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
-import express from 'express';
+import express, { request, response } from 'express';
+
+import './database/connection.ts';
+
+import routes from './routes';
 
 const app = express();
+
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333);
